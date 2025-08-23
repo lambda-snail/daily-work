d0eb0f9a31761031c31db120415c599e0e2e34de
lambda-snail <>
2025-08-23 12:02:40 +0200

    add documentation link to database settings

--- Server/Common/Settings/DatabaseSettings.cs
+++ Server/Common/Settings/DatabaseSettings.cs
@@ -2,7 +2,8 @@ namespace Server.Common.Settings;
 
 public class DatabaseSettings
 {
+    /// <summary>
+    /// For using managed identity with connection string, see https://learn.microsoft.com/en-us/sql/connect/ado-net/sql/azure-active-directory-authentication?view=sql-server-ver16#using-managed-identity-authentication
+    /// </summary>
     public required string ConnectionString { get; set; }
-    //public required string DatabaseName { get; set; }
-    
 }
